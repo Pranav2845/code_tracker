fe68ef6a59391d781effbf3af580ef699846412c
Pranav2845 <pranavpandey9211@gmail.com>
2025-07-08 01:22:03 +0530

    cc

--- backend/routes/platform.js
+++ backend/routes/platform.js
@@ -1,8 +1,8 @@
 // backend/routes/platform.js
 import express from 'express';
-import authMiddleware from '../middleware/auth.js';
+
 import { syncPlatform } from '../controllers/platformController.js';
 
 const router = express.Router();
-router.post('/sync/:platform', authMiddleware, syncPlatform);
+router.post('/sync/:platform', syncPlatform);
 export default router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,4 @@
-// backend/routes/userRoutes.js
+// backend/routes/user.js
 import express from 'express';
 import {
   getUserProfile,
@@ -11,8 +11,7 @@ import {
   getCSESSubmissionCount,
   getCode360TotalCount,
   getCode360SolvedProblems,
-  getCodeChefTotalCount,
-  getCodeChefSolvedProblems,
+
   changePassword
 } from '../controllers/userController.js';
 
@@ -29,8 +28,6 @@ router.get   ('/cses/submissions', getCSESSubmissionCount);
 router.post  ('/change-password', changePassword);
 router.get   ('/code360/problems/:username', getCode360SolvedProblems);
 router.get   ('/code360/count/:username', getCode360TotalCount);
-router.get   ('/codechef/count/:username', getCodeChefTotalCount);
-router.get   ('/codechef/problems/:username', getCodeChefSolvedProblems);
 router.get   ('/cses/problems/:handle', getCSESSolvedProblems);
 
 export default router;
